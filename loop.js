1d9a458c8a083477a239013b7c0387eb27073c66
triptikarn <triptikarn6@gmail.com>
2026-05-06 20:42:08 +0530

    updated sum loop code

--- loop.js
+++ loop.js
@@ -1,94 +1,94 @@
-// ques 1 Print numbers 1 to 10 ?
+// // ques 1 Print numbers 1 to 10 ?
 
-// for(let i = 1; i<=10; i++){
-//     console.log(i);
-// }
+for(let i = 1; i<=10; i++){
+    console.log(i);
+}
 
-// ------------------------------------------------------------
+// // ------------------------------------------------------------
 
-// ques 2 Print even numbers from 1 to 20 ?
+// // ques 2 Print even numbers from 1 to 20 ?
 
-// for(let a = 1; a<=20; a++){
-//     if(a % 2 === 0){
-//         console.log(a);
+for(let a = 1; a<=20; a++){
+    if(a % 2 === 0){
+        console.log(a);
         
-//     }
-// }
+    }
+}
 
-//-------------------------------------------------------
+// //-------------------------------------------------------
 
-//  ques 3 Reverse a number ?
-// let num = 123;
-// let reverse = 0;
-// while(num > 0){
-//     let digit = num % 10;
-//     reverse = reverse*10 + digit;
-//     num = Math.floor(num/10)
-// }
-// console.log(reverse);
+// //  ques 3 Reverse a number ?
+let num = 123;
+let reverse = 0;
+while(num > 0){
+    let digit = num % 10;
+    reverse = reverse*10 + digit;
+    num = Math.floor(num/10)
+}
+console.log(reverse);
 
-//---------------------------------------------------------
+// //---------------------------------------------------------
 
 // ques 4  check even or odd ?
-// let num = 4;
-// if(num % 2 === 0){
-//     console.log("even");  
-// }else{
-//     console.log("odd"); 
-// }
+let num = 4;
+if(num % 2 === 0){
+    console.log("even");  
+}else{
+    console.log("odd"); 
+}
 
-// -------------------------------------------------------
+// // -------------------------------------------------------
 
-// ques 5 Print 1 to 10 number and skip 5 ?
-//  for(let b = 1; b<=10; b++){
-//     if(b===5){
-//         continue;
-//     }console.log(b);
-//  }
+// // ques 5 Print 1 to 10 number and skip 5 ?
+ for(let b = 1; b<=10; b++){
+    if(b===5){
+        continue;
+    }console.log(b);
+ }
 
-// ----------------------------------------------------------
+// // ----------------------------------------------------------
 
-// ques 6 Print odd number ? 
-// for( let a = 1; a<=20; a++){
-//     if(a % 2 !== 0){
-//         console.log(a);
-//     }
-// }
+// // ques 6 Print odd number ? 
+for( let a = 1; a<=20; a++){
+    if(a % 2 !== 0){
+        console.log(a);
+    }
+}
 
-// -------------------------------------------------
+// // -------------------------------------------------
 
 // ques 7 Back count ?
-//  for(let i = 10; i>=1; i--){
-//     console.log("i=",i);
-//  }
+ for(let i = 10; i>=1; i--){
+    console.log("i=",i);
+ }
 
-//--------------------------------------------------
+// //--------------------------------------------------
 
-// ques 8 Print Table of 5 ?
-// for(let i = 1; i<=10; i++){
-// console.log(i*5);
-// }
+// // ques 8 Print Table of 5 ?
+for(let i = 1; i<=10; i++){
+console.log(i*5);
+}
 
-//--------------------------------------------------
+// //--------------------------------------------------
 
-// ques 9  find sum of 1 to 100 ?
-// sum = 0;
-// for( let i = 1; i<=100; i++){
-//     sum+= i;
-// }console.log(sum);
+// // ques 9  find sum of 1 to 100 ?
+sum = 0;
+for( let i = 1; i<=100; i++){
+    sum+= i;
+}console.log(sum);
 
-//-------------------------------------------------
+// //-------------------------------------------------
 
- // ques 10 print factorial of 1 ?
-//  let fact = 1;
-//  for( i = 5; i>=1; i--){
-//     fact*=i;
-//  }console.log(fact);
+//  // ques 10 print factorial of 1 ?
+ let fact = 1;
+ for( i = 5; i>=1; i--){
+    fact*=i;
+ }console.log(fact);
  
 
-//------------------------------------------------------
+// //------------------------------------------------------
 
-// ques 11  matrics ? 
+// // ques 11  matrics ? 
 let n = 12;
 fact = 1;
 
@@ -101,4 +101,41 @@ for(let i=5; i<=1; i++){
         star=star+"*";
     }
     console.log(star);
+}
+
+//  //-------------------------------------------
+
+ // ques 12 print  * ?
+ for(let i = 1; i<=5;i++){
+    let star ="";
+    for(let j=1; j<=i; j++){
+        star=star+"*";
+    }
+    console.log(star);
+ }
+
+// //-------------------------------------------
+
+// // ques 13 reverse * ?
+for(let i = 1; i<=5; i++){
+     let star ="";
+     for(let j=5; j>=i; j--){
+         star=star+"*";
+     }
+    console.log(star);
+}
+
+// //-----------------------------------------------------
+
+// // ques 14 print an isosceles triangle pattern using stars with the help of loops ? 
+for(i=1; i<=5; i++){
+    let patter = "";
+
+    for(let j=1; j<=5-i; j++){
+        patter += " "
+    }
+    for(let k=1; k <= (2*i-1); k++){
+        patter += "*"
+    }
+    console.log(patter);
 }
