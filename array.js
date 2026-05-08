d2b311da730b048d95144f226af469d4416d203f
triptikarn <triptikarn6@gmail.com>
2026-05-08 16:26:28 +0530

    Renew array.js

--- array.js
+++ array.js
@@ -10,4 +10,13 @@ console.log("sum:",sum);
 
 
 
+//2. largest value 
+const num = [10,20,30,40,50];
+let largest = num[0];
+for(let i=1; i<num.length; i++){
+    if(num[i]>largest){
+        largest = num[i];
+    }
+}
+console.log(largest);
 
