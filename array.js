9eeceb276aba470c90c2c4ac5848afab42d9b0f3
triptikarn <triptikarn6@gmail.com>
2026-05-08 16:22:07 +0530

    array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+//1. Write  a javascript program to find the sum of all elements of a for loop . 
+
+const num = [10,20,30,40,50];
+let sum = 0;
+for(let i=0; i<num.length; i++){
+    sum+=num[i];
+    // console.log(num[i]);  
+}
+console.log("sum:",sum);
+
+
+
+
