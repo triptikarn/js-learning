33d4bbadf52975bd731067dbb945ae232fb7592c
triptikarn <triptikarn6@gmail.com>
2026-05-09 12:52:10 +0530

    Added new sorting example

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-//1. Write  a javascript program to find the sum of all elements of a for loop . 
+// //1. Write  a javascript program to find the sum of all elements of a for loop . 
 
 const num = [10,20,30,40,50];
 let sum = 0;
@@ -7,9 +7,9 @@ for(let i=0; i<num.length; i++){
     // console.log(num[i]);  
 }
 console.log("sum:",sum);
+//---------------------------------------------------
 
-
-//2. largest value 
+// //2. largest value 
 const num = [10,20,30,40,50];
 let largest = num[0];
 for(let i=1; i<num.length; i++){
@@ -18,15 +18,17 @@ for(let i=1; i<num.length; i++){
     }
 }
 console.log(largest);
+//-----------------------------------------------
 
-
-// reverse the number 
+// //  3.reverse the number 
 let num = [1,2,3,4,5];
 for(let i = num.length;i>=0;i--){
     console.log(num[i]);
 }
 
-// swap the number
+// ----------------------------------------------------
+
+// // swap the number
 let arr = [40,10,20,30];
 for(let i = 0; i<arr.length;i++){
     for(let j = 0; j<arr.length-1;j++){
@@ -40,16 +42,55 @@ for(let i = 0; i<arr.length;i++){
 }
 console.log(arr);
 
-// let fruits =["apple","mango","banana"]
-// function checkFruit(value){
-//     for(let i=0; i<fruits.length; i++)
-//         {
-//       if(fruits[i]==value){
-//         return true;//found it
-//       }
-//     }
-//     return false; // not found
-// }
-// console.log(checkFruit("tripti"));//true
+//------------------------------------------
+
+//4. Array example 
+ let fruits =["apple","mango","banana"]
+function checkFruit(value){
+    for(let i=0; i<fruits.length; i++)
+        {
+      if(fruits[i]==value){
+        return true;//found it
+      }
+    }
+    return false; // not found
+}
+console.log(checkFruit("tripti"));//true
 // //console.log(checkFruit("oran"));// false
 
+
+// --------------------------------------------
+
+//5. negative numbers
+    
+let arr =[-2,5,-10,8];
+
+for(let i= 0; i < arr.length;i++){
+    for(let j = 0; j < arr.length-1; j++)
+        {
+        if(arr[j] > arr[j+1]){
+
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+console.log(arr);
+
+// -------------------------------------------
+
+//6. Duplicate value 
+let arr = [4,2,4,1];
+
+for (let i = 0 ; i<arr.length; i++){
+    for(let j = 0; j< arr.length-1; j++){
+        if(arr[j] > arr[j+1]){
+            
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp
+        }
+    }
+}
+console.log(arr);
