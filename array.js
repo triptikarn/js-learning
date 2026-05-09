795e76061c250744beda3e2505cd5034b8e9f411
triptikarn <triptikarn6@gmail.com>
2026-05-09 12:23:21 +0530

    updated array file

--- array.js
+++ array.js
@@ -9,7 +9,6 @@ for(let i=0; i<num.length; i++){
 console.log("sum:",sum);
 
 
-
 //2. largest value 
 const num = [10,20,30,40,50];
 let largest = num[0];
@@ -19,4 +18,38 @@ for(let i=1; i<num.length; i++){
     }
 }
 console.log(largest);
+
+
+// reverse the number 
+let num = [1,2,3,4,5];
+for(let i = num.length;i>=0;i--){
+    console.log(num[i]);
+}
+
+// swap the number
+let arr = [40,10,20,30];
+for(let i = 0; i<arr.length;i++){
+    for(let j = 0; j<arr.length-1;j++){
+        if(arr[j]>arr[j+1]){
+            // swap 
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+console.log(arr);
+
+// let fruits =["apple","mango","banana"]
+// function checkFruit(value){
+//     for(let i=0; i<fruits.length; i++)
+//         {
+//       if(fruits[i]==value){
+//         return true;//found it
+//       }
+//     }
+//     return false; // not found
+// }
+// console.log(checkFruit("tripti"));//true
+// //console.log(checkFruit("oran"));// false
 
