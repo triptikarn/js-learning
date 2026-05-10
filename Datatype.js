350464771e6a9ea79f673f91cfb60b1edf0afbf5
triptikarn <triptikarn6@gmail.com>
2026-05-10 16:16:58 +0530

    Concept of Data types in js

---
+++ Datatype.js
@@ -0,0 +1,47 @@
+/**
+ * 1. Premitive data types
+ * there are seven types of 
+ * 1. Number
+ * 2. String
+ * 3. boolean
+ * 4. null
+ * 5. undefiend
+ * 6. symbole
+ * 7. BigInt
+ 
+ * 2. Non Premitive data types
+ * 1. Array
+ * 2. object
+ * 3. Function
+ */
+
+
+//Primitive Data Type:-
+//1.number
+let a = 12;
+console.log(a);
+
+//2.String
+let b = "Ram";
+console.log(b);
+
+// 3.boolean
+let c = true;
+console.log(c);
+
+//4.null
+let a = null;
+console.log(a);
+
+//5.undefined
+let b ;
+console.log(b);
+
+
+//6.symbole
+let c = Symbol("Ambience");
+console.log(c);
+
+//7.BigInt
+let b = BigInt(656464645648676+97+94649876694321654);
+console.log(b);
