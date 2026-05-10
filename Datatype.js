e143deb5bdf73e41ac8ed230b4af8d12ecde31e3
triptikarn <triptikarn6@gmail.com>
2026-05-10 16:33:11 +0530

    primitive and Non-primitive Data type

--- Datatype.js
+++ Datatype.js
@@ -45,3 +45,47 @@ console.log(c);
 //7.BigInt
 let b = BigInt(656464645648676+97+94649876694321654);
 console.log(b);
+
+
+
+//Non-primiitive Datatypes
+//1.Array
+//With the help of an array we can store more than one element under a single name .
+
+const a = [12,45,true,false,"sita","ram",33];
+//console.log(a);
+console.table(a);
+
+
+//2.object
+//An object in JavaScript is an entity having properties and methods. Everything is an object in JavaScript.
+//How to create an object in javaScript:
+
+
+const b = {
+    name:"Aradhya",
+    class:"11", // Json 
+    subject:"Science",
+    Ispass:"true"
+}
+console.table(b);
+
+
+// 3.Function
+//  A function in JavaScript is a reusable block of code that performs a specific task when called.
+
+const n = function(a,b){
+
+  return a+b;
+ }(12,45);
+ console.log(n);
+
+ 
+ 
+ function add(a,b){
+
+
+     return a+b;
+ };
+ console.log(add(12,45));
+ 
