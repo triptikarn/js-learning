445e6f2b439d7cb21d119d0d645ada923a5ce43c
triptikarn <triptikarn6@gmail.com>
2026-05-11 23:59:42 +0530

    updated types shift unshift

--- type.js
+++ type.js
@@ -9,3 +9,20 @@ console.log(animals);
 const plants = ['brocooli,','cauliflower','cabbage'];
 console.log(plants.pop());
 console.log(plants);
+
+// // shift
+const array1 = [1,2,3];
+const firstElement = array1.shift();
+
+console.log(array1);
+
+console.log(firstElement);
+
+// Unshift
+const array1 = [1,2,3];
+console.log(array1.unshift());
+
+console.log(array1);
+
+// console.log(firstElement);
+
