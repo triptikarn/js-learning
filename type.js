41ba19d597333d0b0f209de9db2a436167f361f0
triptikarn <triptikarn6@gmail.com>
2026-05-11 23:52:10 +0530

    array type pop and push

---
+++ type.js
@@ -0,0 +1,11 @@
+// Push 
+const animals = ['pigs','goats','sheep'];
+
+const count = animals.push('cows');
+console.log(count);
+console.log(animals);
+
+// Pop 
+const plants = ['brocooli,','cauliflower','cabbage'];
+console.log(plants.pop());
+console.log(plants);
