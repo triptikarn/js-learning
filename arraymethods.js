9b43b3c8c8e874b39a8bd0c01a16a9b629bac01d
triptikarn <triptikarn6@gmail.com>
2026-05-12 10:09:39 +0530

    Methods of array

---
+++ arraymethods.js
@@ -0,0 +1,13 @@
+// to string
+let names = ["Ram","Mohan","Sita","Ramesh"];
+
+console.log(names.toString(","));
+
+//Map
+let salary = [2000,5000,10000,25000];
+
+salary.map((value)=>{
+    console.log(value+2000);
+    
+})
+
