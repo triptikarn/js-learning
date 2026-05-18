fc14c3e61001c7842050f1ce3d3bbc009a31a75c
triptikarn <triptikarn6@gmail.com>
2026-05-18 23:08:43 +0530

    concept update

--- class.js
+++ class.js
@@ -18,3 +18,41 @@ const user2 = new Person("Mohan", 88);
 user1.geet();
 
 user2.geet();
+
+class Student{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+}
+const names = [
+    "Rahul",
+    "Aman",
+    "Priya",
+    "Neha",
+    "Karan",
+    "Riya",
+    "isha",
+    "Shnya",
+    "sofia",
+    "Siya",
+    "Radhika",
+    "Jyoti",
+    "Jarin",
+    "jaya",
+    "Hema",
+    "Rekha",
+    "Tanshika",
+    "Tanya",
+    "Komal",
+    "Rahul"
+];
+
+const students = [];
+
+for (let i = 0; i<names.length;i++){
+    let student = new Student(names[i],18+i);
+    students.push(student);
+}
+console.log(students);
+
