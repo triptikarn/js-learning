1527f7e6615ab52b4797ffa55bd545bdf4cea1c2
triptikarn <triptikarn6@gmail.com>
2026-05-25 22:06:45 +0530

    promise parts

--- promise.js
+++ promise.js
@@ -15,3 +15,26 @@ const recdata = new Promise((resolve,reject)=>{
 console.log(recdata);
 
 //Promise in Js 
+
+const recedata = new Promise((resolve, reject)=>{
+    
+let user = false;
+
+    if(user){
+        resolve("Welcome")
+    }else{
+        reject("Login Failed....")
+    }
+
+});
+
+recedata.then((data)=>{
+    console.log(data);
+    
+}).catch((err)=>{
+    console.log(err);
+    
+}).finally(()=>{
+    console.log("It is going on...");
+    
+})
