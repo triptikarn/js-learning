06bb66036d3a1cab09c773ded368c0cfb523cbea
triptikarn <triptikarn6@gmail.com>
2026-05-28 10:07:58 +0530

    Added burgundy background hover effect

--- bg.js
+++ bg.js
@@ -12,22 +12,41 @@
 
 // Text change on click.
 
-let heading = document.querySelector("h1");
+// let heading = document.querySelector("h1");
 
-heading.addEventListener("click",()=>{
-    heading.innerText="Ambience Computer education";
+// heading.addEventListener("click",()=>{
+//     heading.innerText="Ambience Computer education";
 
-    heading.style.color = "#d4af37";
+//     heading.style.color = "#d4af37";
 
-    heading.style.backgroundColor = "#1b2a7a";
+//     heading.style.backgroundColor = "#1b2a7a";
 
-    heading.style.border = "3px solid #1b2a7a";
+//     heading.style.border = "3px solid #1b2a7a";
 
-    heading.style.padding = "20px";
+//     heading.style.padding = "20px";
 
-    heading.style.borderRadius = "15px";
+//     heading.style.borderRadius = "15px";
 
-    heading.style.textAlign = "center";
+//     heading.style.textAlign = "center";
 
-    heading.style.boxShadow = "0px 0px 20px #1b2a7a";
-});
+//     heading.style.boxShadow = "0px 0px 20px #1b2a7a";
+// });
+
+
+let btn = document.querySelector("button");
+
+        btn.addEventListener("mouseover", () => {
+
+            document.body.style.backgroundColor = "#800020";
+
+            document.body.style.color = "white";
+
+        });
+
+        btn.addEventListener("mouseleave", () => {
+
+            document.body.style.backgroundColor = "white";
+
+            document.body.style.color = "black";
+
+        });
